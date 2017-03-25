e63e1f7c94c79ddd4d931cc5f1487e70b09cd34a
Nikita Nikitin <berozzy@gmail.com>
2017-03-25 14:09:50 +0100

    refactored and updated tests

--- tests/adapters.test.js
+++ tests/adapters.test.js
@@ -1,43 +1,74 @@
 import assert from 'assert'
 
 import rest from '../src/core'
-import soundcloudAdapter from '../src/adapters/soundcloud'
 
 describe('Checking adapters:', () => {
   describe('rest.registerAdapter()', () => {
-    it('should throw an error when passing not a function', () => {
-      let executable = rest.registerAdapter
+    describe('should throw an error', () => {
+      it('when passing not a function', () => {
+        let executable = rest.registerAdapter
 
-      assert.throws(executable, TypeError)
+        assert.throws(executable, /Adapter must be a function/)
+      })
+
+      it('when passing an empty function', () => {
+        let executable = () => {
+          rest.registerAdapter(function () { })
+        }
+
+        assert.throws(executable, /Adapter must return a valid object/)
+      })
+
+      it('when there is no `authorize` method in adapter constructor', () => {
+        let executable = () => {
+          rest.registerAdapter(function () { return { name: 'test' } })
+        }
+
+        assert.throws(executable, /Adapter should contain "authorize" method/)
+      })
+
+      it('when there is no `name` specified', () => {
+        let executable = () => {
+          rest.registerAdapter(function () { return { authorize: () => {} } })
+        }
+
+        assert.throws(executable, /Adapter should contain the "name" property/)
+      })
     })
 
-    it('should throw an error when passing an empty function', () => {
-      let executable = () => {
-        rest.registerAdapter(function () { })
-      }
+    describe('should not add a new adapter to the list of all adapters, ', () => {
+      it('if there is no name specified', () => {
+        let brokenAdapter = { authorize() {} }
+        let executable = () => {
+          rest.registerAdapter(function () { return brokenAdapter })
+        }
 
-      assert.throws(executable, TypeError)
+        assert.throws(executable, /Adapter should contain the "name" property/)
+        assert.equal(rest.adapters.indexOf(brokenAdapter), -1)
+      })
+
+      it('if there is no authorize method', () => {
+        let brokenAdapter = { name: 'brokenAdapter1' }
+        let executable = () => {
+          rest.registerAdapter(function () { return brokenAdapter })
+        }
+
+        assert.throws(executable, /Adapter should contain "authorize" method/)
+        assert.equal(rest.adapters.indexOf(brokenAdapter), -1)
+      })
+
+      it('if the authorize method is not executable', () => {
+        let brokenAdapter = { name: 'brokenAdapter2', authorize: undefined }
+        let executable = () => {
+          rest.registerAdapter(function () { return brokenAdapter })
+        }
+
+        assert.throws(executable, /"authorize" method must be a function/)
+        assert.equal(rest.adapters.indexOf(brokenAdapter), -1)
+      })
     })
 
-    it('should throw an error when there is no `authorize` method in adapter constructor', () => {
-      let executable = () => {
-        rest.registerAdapter(function () { return { } })
-      }
-
-      assert.throws(executable, Error)
-    })
-
-    it('should not add a new adapter to the list of all adapters, if there are any errors', () => {
-      let brokenAdapter = { name: 'brokenAdapter1' }
-      let executable = () => {
-        rest.registerAdapter(function () { return brokenAdapter })
-      }
-
-      assert.throws(executable, Error)
-      assert.equal(rest.adapters.indexOf(brokenAdapter), -1)
-    })
-
-    it('should execute without any errors when passing proper function', () => {
+    it('should execute without any errors when passing a proper function', () => {
       let executable = () => {
         rest.registerAdapter(function () {
           return {
