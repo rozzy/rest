c4ae14b8c32d59fe4f7b07a8e2b57536c4bda502
Nikita Nikitin <berozzy@gmail.com>
2017-04-19 11:22:53 +0200

    moved test scripts from index.js to examples folder

---
+++ tests/examples/soundcloud/index.js
@@ -0,0 +1,72 @@
+import rest from '../../../index'
+
+import credits from '../../../credits'
+
+var bot = rest.new({
+  adapter: 'soundcloud', // twitter, instagram, youtube, custom
+  threads: 1,
+  authorization: {
+    clientId: credits.sc.CLIENT_ID,
+    clientSecret: credits.sc.CLIENT_SECRET,
+    redirectURI: 'http://localhost:8080/callback.html',
+  },
+  limits: {
+    callsPerPeriod: 15000,
+    period: 86400
+  }
+})
+
+bot
+  .registerMethods((restSettings, instance) => {
+    return function authorize() {
+      // example of calling super method from the adapter
+      this._adapter.methods.authorize.apply(this, arguments)
+    }
+  })
+  .registerMethods((restSettings, instance) => {
+    return {
+      listenNext(prevResolution, index, done) {
+        if (instance.data.loaded <= instance.data.available) {
+          return ['loadNext', 'listenNext']
+        } else {
+          let track = foundNextTrack(instance)
+          listenToTheTrack(track)
+
+          return true
+        }
+      }
+    }
+  })
+  .loadPatterns((restSettings, instance) => {
+    return [
+      // {
+      //   name: 'main',
+      //   sequence: [
+      //     ':explore',
+      //     (prevResolution, index, done) => {
+      //       if (prevResolution === true) {
+      //         return ':listenToNewTracks'
+      //       } else {
+      //         return false
+      //       }
+      //     },
+      //     (prevResolution, index, done, sequencer) => {
+      //       if (prevResolution === true) return sequencer.repeat()
+      //     }
+      //   ]
+      // },
+      {
+        name: 'explore',
+        sequence: ['listenNext']
+      }
+    ]
+  })
+  .registerMethods((restSettings, instance) => {
+    return {
+      chooseCriterias(prevResolution, index, done) {
+        console.log('chooseCriterias ...', prevResolution, index)
+        setTimeout(done, 1000)
+      }
+    }
+  })
+  .run()
